7969a508a7bfdf2c513541f12023c5aec7c51837
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-08-14 11:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function isSameType(value1, value2) {
     if ((Number.isNaN(value1) && Number.isNaN(value2)) || (typeof value1 === typeof value2)) {
         return true;
     }
+	else if (Number.isNaN(value1) || Number.isNaN(value2)) {
+        return false;
+	}
+
     else {
         return false;
     }
